428b1e5f45d5c91740ba78093f9b7a5de2e442a3
Sandip123samanta <111598408+Sandip123samanta@users.noreply.github.com>
2023-11-28 23:56:10 +0530

    added the nav bar

--- src/components/common/navbar/index.js
+++ src/components/common/navbar/index.js
@@ -1,55 +1,68 @@
 "use client";
-import Image from 'next/image'
-import React, { useState,useEffect } from 'react'
-import styles from './style.module.scss'
-import Nav from './nav';
-import { AnimatePresence } from 'framer-motion';
-import { usePathname } from 'next/navigation';
-import { motion } from 'framer-motion';
-import GalaxyBtn from '../Buttons/galaxyButton/galaxyBtn';
-import logo from '../../../../public/logo2.png'
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import styles from "./style.module.scss";
+import Nav from "./nav";
+import { AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+import logo from "../../../../public/logo2.png";
+import useReadingProgress from "@/components/utils/useReadingProgress";
 import {
-	slideInFromLeft,
-	slideInFromRight,
-	slideInFromTop,
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
 } from "../../utils/motion";
+// import GlowBtn from "../Buttons/glowbtn/glowBtn";
 
 function Home() {
-    const [isActive,setIsActive]=useState(false)
-    const pathname = usePathname();
+  const completion = useReadingProgress();
+  const [isActive, setIsActive] = useState(false);
+  const pathname = usePathname();
 
-    useEffect( () => {
-        if(isActive) setIsActive(false)
-    }, [pathname])
+  useEffect(() => {
+    if (isActive) setIsActive(false);
+  }, [pathname]);
   return (
     <>
+      <motion.div initial="hidden" animate="visible">
         <motion.div
-        initial='hidden'
-        animate='visible'
+          variants={slideInFromTop}
+          className="one w-screen h-15 overflow-hidden flex justify-between items-center p-6 text-white z-[10] fixed top-0 left-0"
         >
-        <motion.div 
-        variants={slideInFromTop}
-        className='one w-screen h-15 overflow-hidden flex justify-between items-center p-6 text-white z-[10] fixed top-0 left-0'>
-            <div className={`${styles.logo} relative w-[8vw]`}>
-                <Image className='object-cover w-full' src={logo} alt='logo'/>
+          <div className={`${styles.logo} relative w-[6vw]`}>
+            <Image className="object-cover w-full" src={logo} alt="logo" />
+          </div>
+          <div className="w-[20vw] h-full flex items-center justify-end gap-5">
+            <div className="h-full z-10">
+                <button className="text-white lg:text-[1.5vw] text-[4vw] border border-purple-500 rounded-lg p-1">
+                    Brochure
+                </button>
             </div>
-            {/* <div className='w-[20vw] h-full flex items-center justify-end gap-5'>
-                <div className='h-full z-10'>
-                    <GalaxyBtn/>
-                </div>
-                <div className={styles.header}>
-                <div onClick={()=>{setIsActive(!isActive)}} className={styles.button}>
-                        <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
-                    </div>
-                </div>
-            </div> */}
+            <div className={styles.header}>
+              <div
+                onClick={() => {
+                  setIsActive(!isActive);
+                }}
+                className={styles.button}
+              >
+                <div
+                  className={`${styles.burger} ${
+                    isActive ? styles.burgerActive : ""
+                  }`}
+                ></div>
+              </div>
+            </div>
+          </div>
+          <span
+          style={{transform: `translateX(${completion - 100}%)`}}
+          className="absolute bg-gradient-to-r from-[#090979] to-[#0088ff] h-1 w-full top-0 left-0"
+          />
         </motion.div>
-        </motion.div>
-        <AnimatePresence mode="wait">
-            {isActive && <Nav />}
-        </AnimatePresence>
+      </motion.div>
+      <AnimatePresence mode="wait">{isActive && <Nav />}</AnimatePresence>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
