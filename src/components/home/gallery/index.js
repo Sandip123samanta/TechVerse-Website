24081b0493b21dc2b8628a58696428507ac66f45
Sandip123samanta <111598408+Sandip123samanta@users.noreply.github.com>
2023-11-28 19:54:41 +0530

    Added the gallery

--- src/components/home/gallery/index.js
+++ src/components/home/gallery/index.js
@@ -1,6 +1,6 @@
-'use client'
-import React, { useEffect } from 'react'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import Image from "next/image";
 import {
   floating1,
   floating2,
@@ -9,44 +9,122 @@ import {
   floating5,
   floating6,
   floating7,
-  floating8
-} from './images'
-import './style.css'
+  floating8,
+} from "./images";
+import "./style.css";
 
-const DepthOfFieldGallery = () => {
-  useEffect(() => {
-    const photosDom = document.getElementById('photos');
-    const images = photosDom.getElementsByTagName('img');
-    const len = images.length;
-    const deg = Math.floor(360 / len);
-
-    for (let i = 0; i < len; i++) {
-      images[i].style = `transform : rotateY(${deg * i}deg) translateZ(380px)`;
-    }
-
-    let x = 0;
-    const intervalId = setInterval(() => {
-      photosDom.style.transform = `rotateX(-10deg) rotateY(${(x++) * 0.2}deg)`;
-    }, 30);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
+function Gallery() {
   return (
-    <div className="depth-of-field">
-      <div id="photos">
-        <Image src={floating1} />
-        <Image src={floating2} />
-        <Image src={floating3} />
-        <Image src={floating4}/>
-        <Image src={floating5}/>
-        <Image src={floating6}/>
-        <Image src={floating7}/>
-        <Image src={floating8}/>
-        
+    <>
+      <h1 className="text-white text-center text-[5vw] font-bold galleryText">Gallery</h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-screen h-screen p-6">
+        <div className="grid gap-4">
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear opacity-[.7]"
+              src={floating1}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating2}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating3}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating4}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating5}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating6}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating7}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating8}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating4}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating6}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7]"
+              src={floating2}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+          <div className="relative overflow-hidden">
+            <Image
+              className="h-auto max-w-full rounded-lg hover:scale-[1.2] transition-all duration-300 ease-linear  opacity-[.7] "
+              src={floating1}
+              alt="galleryImage"
+              fill={true}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
-};
+}
 
-export default DepthOfFieldGallery;
+export default Gallery;
