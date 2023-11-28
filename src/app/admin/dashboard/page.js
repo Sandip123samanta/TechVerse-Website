4a7b50b8a689a636eecbf06c5fd8682e6d3a2aec
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2023-11-28 13:49:49 +0530

    error solved15

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,36 +1,43 @@
 "use client";
 // pages/index.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  const [responseData, setResponseData] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [responseData, setResponseData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/api/admin'); // Define your API route
-        setResponseData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				setIsLoading(true)
+				const response = await axios.get("/api/admin");
+				console.log(response); // Define your API route
+				setResponseData(response.data);
+				setIsLoading(false)
+			} catch (error) {
+				setIsLoading(false)
+				console.error("Error fetching data:", error.message);
+			}
+		};
+		console.log("in Fetch Data..");
+		fetchData();
+	}, []);
 
-    fetchData();
-  }, []);
-
-  return (
-    <div className='mt-96 text-xl text-white'>
-      <h1>Data Dashboard</h1>
-      <ul>
-        {responseData.map((item) => (
-          <li key={item._id}>
-            {item.name}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className="mt-96 text-xl text-white">
+			<h1>Data Dashboard</h1>
+			{isLoading ? (
+				<h1 className=" text-3xl font-serif mt-48">Loading...</h1>
+			) : (
+				<div className=" w-5/6 md:flex flex-col gap-3">
+					{responseData.map((item) => (
+						<h1 key={item._id}>{item.name}</h1>
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default Home;
