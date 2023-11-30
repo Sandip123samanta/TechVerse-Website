32291551ccc235b01c4b7b4c7621d76f3acea3a8
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2023-11-30 12:52:46 +0530

    error solved22

--- src/app/api/admin/route.js
+++ src/app/api/admin/route.js
@@ -1,15 +1,23 @@
 import { connectDB } from "@/app/config/dbConfig";
 import { EventRegistration } from "@/app/models/participant";
-import { NextRequest,NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 connectDB();
 
 export async function GET(request) {
-    try {
-        const registrations = await EventRegistration.find();
-        return NextResponse.json(registrations);
-    } catch (error) {
-        console.log("Error fetching registrations.", error);
-        return NextResponse.json(error);
-    }
+	try {
+		const registrations = await EventRegistration.find();
+
+		// Set Cache-Control header to prevent caching
+		const response = NextResponse.json(registrations);
+		response.headers.set(
+			"Cache-Control",
+			"no-cache, no-store, must-revalidate"
+		);
+
+		return response;
+	} catch (error) {
+		console.log("Error fetching registrations.", error);
+		return NextResponse.json(error);
+	}
 }
