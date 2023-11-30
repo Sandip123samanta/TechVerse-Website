01e2860a80cbbb7f7f21f6344c813c3b9f92e536
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2023-11-30 12:38:55 +0530

    error solved21

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,25 +1,36 @@
 // pages/dashboard.js
-"use client"
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import EventTable from './EventTable';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import EventTable from "./EventTable";
 
 const Dashboard = () => {
-  const [events, setEvents] = useState([]);
+	const [registrations, setRegistrations] = useState([]);
+	const [registrationCount, setRegistrationCount] = useState(0);
 
-  useEffect(() => {
-    // Replace with your API endpoint for fetching events
-    axios.get('/api/admin').then((response) => {
-      setEvents(response.data);
-    });
-  }, []);
+	useEffect(() => {
+		// Fetch data from your API endpoint
+		const fetchData = async () => {
+			try {
+				const response = await axios.get("/api/admin");
+				setRegistrations(response.data);
+				console.log(response.data);
+				setRegistrationCount(response.data.length);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+		fetchData();
+	}, []);
 
-  return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-2xl font-bold mb-4">Event Management Dashboard</h1>
-      <EventTable events={events} />
-    </div>
-  );
+	return (
+		<div className="container mx-auto p-8">
+			<h1 className="text-2xl font-bold mb-4">
+				Event Management Dashboard
+			</h1>
+			<EventTable events={registrations} />
+		</div>
+	);
 };
 
 export default Dashboard;
